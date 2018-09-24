02e426b64c43906c9fc06a002b9d1b172ef58a8e
Kevin Red <kred@uacurrents.org>
2018-09-24 11:15:00 -0500

    Add project files.

---
+++ GitHubHello/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            WaitConsole();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Just wanted to say hello.");
+        }
+
+        static void WaitConsole()
+        {
+            Console.WriteLine("Press Enter...");
+            Console.Read();
+        }
+    }
+}
